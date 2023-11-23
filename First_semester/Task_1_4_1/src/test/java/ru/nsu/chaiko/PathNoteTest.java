0447c7292e93bcbdbeaec91ec63e321b1ca1f813
i.chaiko <i.chaiko@g.nsu.ru>
2023-11-23 11:42:26 +0700

    CS fix 4

--- First_semester/Task_1_4_1/src/test/java/ru/nsu/chaiko/PathNoteTest.java
+++ First_semester/Task_1_4_1/src/test/java/ru/nsu/chaiko/PathNoteTest.java
@@ -22,11 +22,11 @@ public class PathNoteTest {
     void test2() {
         PathNote pathNote = new PathNote("abc", "dcvv");
 
-        Assertions.assertThrows(UniverseException.class, ()->{
+        Assertions.assertThrows(UniverseException.class, () -> {
             pathNote.setMark(2, -5, "a");
         });
 
-        Assertions.assertThrows(UniverseException.class, ()->{
+        Assertions.assertThrows(UniverseException.class, () -> {
             pathNote.setMark(0, 4, "a");
         });
     }
