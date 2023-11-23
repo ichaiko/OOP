05331f6bab7af53f4c5e6153879b15dd445425bc
i.chaiko <i.chaiko@g.nsu.ru>
2023-11-23 11:34:26 +0700

    CS fix 2.1

--- First_semester/Task_1_4_1/src/main/java/ru/nsu/chaiko/PathNote.java
+++ First_semester/Task_1_4_1/src/main/java/ru/nsu/chaiko/PathNote.java
@@ -4,55 +4,17 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Objects;
 
-class NoMarksException extends Exception {
-    public NoMarksException(String message) {
-        super(message);
-    }
-}
-
-class NoSemesterException extends Exception {
-    public NoSemesterException(String message) {
-        super(message);
-    }
-}
-
-enum Grade {
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5);
-
-    private final int numericalGrade;
-
-    Grade(int grade) {
-        this.numericalGrade = grade;
-    }
-
-    int getValue() {
-        return numericalGrade;
-    }
-
-    public static Grade setValue(int value) throws NoMarksException {
-        for (Grade grade : Grade.values()) {
-            if (value == grade.numericalGrade) {
-                return grade;
-            }
-        }
-
-        throw new NoMarksException("No Grade for this value");
-    }
-}
-
 /**
  * main class of the task.
  */
 public class PathNote {
-    final private String name, surname;
+    private final String name;
+    private final String surname;
 
     /**
      * at the beginning every value is null, because no mark per specific semester.
      */
-    final private HashMap<String, Grade[]> pathNote = new HashMap<>();
+    private final HashMap<String, Grade[]> pathNote = new HashMap<>();
 
     public PathNote(String name, String surname) {
         this.name = name;
@@ -82,11 +44,11 @@ public class PathNote {
     /**
      * sets mark per specific semester.
      */
-    void setMark(int semester, int mark, String subject) throws NoMarksException, NoSemesterException {
+    void setMark(int semester, int mark, String subject) throws UniverseException {
         Grade[] semesters;
 
         if (semester > 8 || semester < 1) {
-            throw new NoSemesterException("incorrect semester value");
+            throw new UniverseException("incorrect semester value");
         }
 
         if (pathNote.containsKey(subject)) {
@@ -102,27 +64,24 @@ public class PathNote {
     /**
      * Calculates avg grade.
      */
-    double getAverageScore() throws NoMarksException {
+    double getAverageScore() throws UniverseException {
         var keys = pathNote.keySet();
-        double wholeGradesCount = 0, wholeGradesSum = 0;
+        double wholeGradesCount = 0;
+        double wholeGradesSum = 0;
 
-        if (keys.isEmpty())
-            throw new NoMarksException("you have no marks!");
+        if (keys.isEmpty()) {
+            throw new UniverseException("you have no marks!");
+        }
 
         for (String subj : keys) {
             var semesters = pathNote.get(subj);
 
             // здесь потоки притянуты за уши, тем более с ними по ассимтотики даже хуже выходит
             // просто полезная тема, захотелось изучить, потрогать
-            // можно было применять стримы сразу к мапе, но для первого знакомство too much, как по мне
-            wholeGradesSum += Arrays.stream(semesters)
-                    .filter(Objects::nonNull)
-                    .mapToDouble(Grade::getValue)
-                    .sum();
+            // можно было применять стримы сразу к мапе, но для первого знакомство too much
+            wholeGradesSum += Arrays.stream(semesters).filter(Objects::nonNull).mapToDouble(Grade::getValue).sum();
 
-            wholeGradesCount += (double) Arrays.stream(semesters)
-                    .filter(Objects::nonNull)
-                    .count();
+            wholeGradesCount += (double) Arrays.stream(semesters).filter(Objects::nonNull).count();
         }
 
         return wholeGradesSum / wholeGradesCount;
@@ -131,9 +90,13 @@ public class PathNote {
     /**
      * returns possibility of getting increased scholarships.
      */
-    boolean increasedScholarships() {
+    boolean increasedScholarships() throws UniverseException {
         int currentSemester = currentSemester();
         var values = pathNote.values();
+
+        if (values.isEmpty()) {
+            throw new UniverseException("no semester marks");
+        }
 
         for (var value : values) {
             if (value[currentSemester] != Grade.FIVE) {
@@ -150,20 +113,52 @@ public class PathNote {
      * condition for getting red diploma:
      * no more, than three '3' grades and avg score not less than 4.75.
      */
-    boolean redDiploma() throws NoMarksException {
+    boolean redDiploma() throws UniverseException {
         var keys = pathNote.keySet();
         int threeCounter = 0;
 
         for (String key : keys) {
             var semester = pathNote.get(key);
 
-            threeCounter += Arrays.stream(semester)
-                    .filter(Objects::nonNull)
-                    .mapToInt(Grade::getValue)
-                    .filter(mark -> mark == 3)
-                    .count();
+            threeCounter += Arrays.stream(semester).filter(Objects::nonNull).mapToInt(Grade::getValue).filter(mark -> mark == 3).count();
         }
 
         return threeCounter < 4 && getAverageScore() >= 4.75;
+    }
+}
+
+/**
+ * class for PathNote exceptions.
+ */
+class UniverseException extends Exception {
+    public UniverseException(String message) {
+        super(message);
+    }
+}
+
+/**
+ * grades list.
+ */
+enum Grade {
+    TWO(2), THREE(3), FOUR(4), FIVE(5);
+
+    private final int numericalGrade;
+
+    Grade(int grade) {
+        this.numericalGrade = grade;
+    }
+
+    int getValue() {
+        return numericalGrade;
+    }
+
+    public static Grade setValue(int value) throws UniverseException {
+        for (Grade grade : Grade.values()) {
+            if (value == grade.numericalGrade) {
+                return grade;
+            }
+        }
+
+        throw new UniverseException("No Grade for this value");
     }
 }
