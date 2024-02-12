c58fd2fe24f1f72b088e9c4470e13dc72ca35f30
i.chaiko <i.chaiko@g.nsu.ru>
2024-02-12 13:18:48 +0700

    first version

---
+++ Task_2_1_1/src/main/java/ru/nsu/chaiko/PrimeNumberCheck.java
@@ -0,0 +1,31 @@
+package ru.nsu.chaiko;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * class for static methods/variables.
+ */
+public class PrimeNumberCheck {
+    public static AtomicBoolean flag = new AtomicBoolean(false);
+
+    /**
+     * for separate numbers.
+     * @param number input number.
+     * @return Does specific number is prime.
+     */
+    public static boolean isNumberNotPrime(int number) {
+        if (number == 0 || number == 1) {
+            return false;
+        }
+
+        int lim = (int) Math.ceil(Math.sqrt(number));
+
+        for (int i = 2; i < lim; i++) {
+            if (number % i == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Task_2_1_1/src/main/java/ru/nsu/chaiko/SolutionsForPrimeChecking.java
@@ -0,0 +1,89 @@
+package ru.nsu.chaiko;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main class of the task.
+ */
+public class SolutionsForPrimeChecking {
+    private final int[] inputArray;
+
+    /**
+     * class's constructor.
+     * @param inputArray array that given as input.
+     */
+    public SolutionsForPrimeChecking(int[] inputArray) {
+        this.inputArray = inputArray;
+    }
+
+    /**
+     * sequential finding not prime number.
+     * @return is there prime number.
+     */
+    boolean sequentialSolution() {
+        boolean res;
+
+        for (int number : this.inputArray) {
+            res = PrimeNumberCheck.isNumberNotPrime(number);
+
+            if (res) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * finding primes using 'numOfThreads' threads.
+     * @param numOfThreads parameter.
+     * @return is there prime number.
+     * @throws InterruptedException for thread stopping.
+     */
+    boolean multiThreadSolution(int numOfThreads) throws InterruptedException {
+        int toEach = this.inputArray.length / numOfThreads;
+        int counter = 0;
+        ArrayList<ThreadForPrimes> list = new ArrayList<>();
+
+        for (int i = 0; i < numOfThreads; i++) {
+            int[] arrayPart = new int[toEach];
+
+            for (int j = 0; j < toEach; j++) {
+                if (counter >= this.inputArray.length) {
+                    break;
+                }
+
+                arrayPart[j] = this.inputArray[counter++];
+            }
+
+            ThreadForPrimes thread = new ThreadForPrimes(arrayPart);
+            thread.start();
+            list.add(thread);
+        }
+
+        for (ThreadForPrimes i : list) {
+            i.join();
+        }
+
+        return PrimeNumberCheck.flag.get();
+    }
+
+    /**
+     * finding prime numbers, using parallel stream.
+     * @return is there prime number.
+     */
+    boolean parallelStreamSolution() {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i : this.inputArray) {
+            list.add(i);
+        }
+
+        long res = list.parallelStream()
+                .filter(PrimeNumberCheck::isNumberNotPrime)
+                .count();
+
+        return res > 0;
+    }
+}
