c0e04ee263c738098821501cdcef4840cb9aef8f
igor-chaiko <igor-chaiko@users.noreply.github.com>
2024-05-30 15:41:21 +0000

    Deploying to gh-pages from @ igor-chaiko/OOP@6bca14e42038c55ccf0a907530bb0192c3cfc486 🚀

---
+++ Task_2_4_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.chaiko"},{"l":"ru.nsu.chaiko.dsl"},{"l":"ru.nsu.chaiko.git"},{"l":"ru.nsu.chaiko.utils"}];updateSearchResults();
