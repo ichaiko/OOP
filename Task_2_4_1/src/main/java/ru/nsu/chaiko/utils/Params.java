cbe11569d7201736f7a53c683c1693d974cd02ff
i.chaiko <i.chaiko@g.nsu.ru>
2024-05-31 23:59:44 +0700

    deleted 2_4_1

--- Task_2_4_1/src/main/java/ru/nsu/chaiko/utils/Params.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.chaiko.utils;
-
-/**
- * params.
- */
-public class Params {
-    public static final String PATHTOCLONE = "src/main/resources/students";
-    public static final String CONFIGPATH = "src/main/resources/scripts/config.groovy";
-}
